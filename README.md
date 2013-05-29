5ad3055c4f1d047f024a7ca0c1851cb7b1c49de9
alexanderkjones <alexanderkjones@gmail.com>
2013-05-29 10:35:36 -0400

    Initial Rails Install

---
+++ README.md
@@ -0,0 +1,3 @@
+README
+
+This is a clean install of Rails 3. Use Wisely :]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreshRails::Application.config.secret_token = '6743a65fc701c6d83be13ebe7a460d19df4b511fe973b75d24e07a8a6869964b7925fc758c9b8a363f3e8bd405ccc52d3bfcd03aae8f35d2e1d59e75985ac713'
